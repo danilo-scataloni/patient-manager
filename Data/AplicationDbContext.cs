fc21073290b185553738072cd07354a940c621c0
danilo.scataloni <danilomsca@gmail.com>
2025-06-19 20:06:14 -0300

    WIP: add support to controllers, eliminate garbage and start to configure SQLite

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using pacient_manager.Models;
+
+namespace pacient_manager.Data;
+
+public class AplicationDbContext : DbContext
+{
+    public AplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    public DbSet<Pacient> Pacients { get; set; }
+}
