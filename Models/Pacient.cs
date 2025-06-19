133b2ce6bb39658f5fc46f3967eda3f9b8777b77
danilo.scataloni <danilomsca@gmail.com>
2025-06-19 20:06:55 -0300

    feat: create Pacient Model

---
+++ Models/Pacient.cs
@@ -0,0 +1,19 @@
+namespace pacient_manager.Models;
+
+public class Pacient
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string  LastName { get; set; }
+    public int Document { get; set; }
+    public string DateOfBirth { get; set; }
+    
+    public Pacient(int id, string firstName, string lastName, int document, string dateOfBirth)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Document = document;
+        DateOfBirth = dateOfBirth;
+    }
+}
