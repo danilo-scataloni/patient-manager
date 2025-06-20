f3ac6441274c8b7dc7d6477520699ec487f5bdd2
danilo.scataloni <danilomsca@gmail.com>
2025-06-19 21:47:31 -0300

    fix: ignore id parameter when creating user (let db random create it)

--- Models/Pacient.cs
+++ Models/Pacient.cs
@@ -1,13 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace pacient_manager.Models;
 
 public class Pacient
 {
+    [JsonIgnore]
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string  LastName { get; set; }
     public int Document { get; set; }
     public string DateOfBirth { get; set; }
     
+    public Pacient() {}
     public Pacient(int id, string firstName, string lastName, int document, string dateOfBirth)
     {
         Id = id;
