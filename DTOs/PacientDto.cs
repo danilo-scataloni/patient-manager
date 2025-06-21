cd64b04952febc8ee27757c278847070989b25dd
danilo.scataloni <danilomsca@gmail.com>
2025-06-20 23:08:01 -0300

    feat: create DTO to update Pacients

---
+++ DTOs/PacientDto.cs
@@ -0,0 +1,9 @@
+namespace pacient_manager.DTOs;
+
+public class PacientDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int? Document { get; set; }
+    public string? DateOfBirth { get; set; }
+}
