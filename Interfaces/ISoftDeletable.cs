b3ada70067ef3d50dfa4728124a9716ff4121fcb
danilo.scataloni <danilomsca@gmail.com>
2025-07-15 13:37:51 -0300

    feat: add BaseEntity and ISoftDeletable interface for soft delete and trackable entities

---
+++ Interfaces/ISoftDeletable.cs
@@ -0,0 +1,6 @@
+namespace patient_manager.Interfaces;
+
+public interface ISoftDeletable
+{
+    DateTime? DateDeleted { get; set; }
+}
---
+++ Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+using patient_manager.Interfaces;
+
+namespace patient_manager.Models;
+
+public class BaseEntity : ISoftDeletable
+{
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public DateTime? DateDeleted { get; set; }
+}
