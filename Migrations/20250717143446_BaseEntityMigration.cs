e5824b2169f9d83420b2afea5f9753b6cb692fa9
danilo.scataloni <danilomsca@gmail.com>
2025-07-17 11:36:22 -0300

    feat: add DateCreated, DateUpdated, and DateDeleted columns to Patients table migration

---
+++ Migrations/20250717143446_BaseEntityMigration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace patient_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseEntityMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Patients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "Patients",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Patients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Patients");
+        }
+    }
+}
