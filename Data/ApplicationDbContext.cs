14375ff37dc0ad17e3bb07f14b49d3bc05687fa0
danilo.scataloni <danilomsca@gmail.com>
2025-08-06 14:03:35 -0300

    feat: implement soft delete functionality for Patient entity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using patient_manager.Models;
 
@@ -5,9 +6,20 @@ namespace pacient_manager.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    
     public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
         
     }
     public DbSet<Patient> Patients { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            modelBuilder
+                .Entity<Patient>()
+                .HasQueryFilter(p => p.DateDeleted == null);
+        }
+    }
 }
--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -2,7 +2,7 @@ using patient_manager.Interfaces;
 
 namespace patient_manager.Models;
 
-public class BaseEntity : ISoftDeletable
+public abstract class BaseEntity : ISoftDeletable
 {
     public DateTime DateCreated { get; set; } = DateTime.Now;
     public DateTime DateUpdated { get; set; } = DateTime.Now;
